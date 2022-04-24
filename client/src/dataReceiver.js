cb5651c2a56ed9fdfc82d0c23cd05ae45dfdc449
Saibaken <shelamov.s@gmail.com>
2022-04-24 14:19:09 +0300

    feat: :package: Добавил необходимые файлы для PWA

--- client/src/dataReceiver.js
+++ client/src/dataReceiver.js
@@ -1,5 +1,5 @@
 export const fetchData = async () => {
-  const response = await fetch("http://localhost:3001/getData").then((response) =>
+  const response = await fetch("http://192.168.1.34:3001/getData").then((response) =>
     response.json()
   );
   response.forEach(flower => {
@@ -9,7 +9,7 @@ export const fetchData = async () => {
 };
 
 export const sendData = async (data) => {
-  const response = await fetch("http://localhost:3001/updateData", {
+  const response = await fetch("http://192.168.1.34:3001/updateData", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
