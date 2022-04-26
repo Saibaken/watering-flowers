c21e632f12b80fd5366177e7e11976b74f82b22f
Saibaken <shelamov.s@gmail.com>
2022-04-26 17:14:42 +0300

    test

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const app = express();
 const port = 3001;
 
-const {getData, updateData} = require('./filemanagement')
+const {getData, updateData} = require('./filemanagement');
+const { response } = require('express');
 
 
 app.use(express.json());
@@ -17,6 +18,10 @@ app.post('/updateData', (request, response) => {
     updateData(request.body);
     response.sendStatus(200);
 });
+
+app.post('/test', (request, response) => {
+    response.send('Test message');
+}) 
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
